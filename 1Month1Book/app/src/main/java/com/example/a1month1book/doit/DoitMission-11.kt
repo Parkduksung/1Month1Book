6d516578f7e6876b2b04390b849fb6f24aaa5855
Parkduksung <duksung1234@naver.com>
2020-11-26 22:48:43 +0900

    [Do-it]Service 1번문제.

---
+++ 1Month1Book/app/src/main/java/com/example/a1month1book/doit/DoitMission-11.kt
@@ -0,0 +1,62 @@
+package com.example.a1month1book.doit
+
+import android.app.Service
+import android.content.Intent
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import com.example.a1month1book.R
+import com.example.a1month1book.base.BaseActivity
+import com.example.a1month1book.databinding.DoitMission11Binding
+
+class DoitMission11 : BaseActivity<DoitMission11Binding>(R.layout.doit_mission_11) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.sendServiceMessageButton.setOnClickListener {
+            Intent(this, DoitMission11Service::class.java).apply {
+                putExtra("message1", binding.inputServiceMessage.text.toString())
+            }.also { intent ->
+                startService(intent)
+            }
+            Log.d("영역:메세지보냄", binding.inputServiceMessage.text.toString())
+        }
+
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        if (intent != null) {
+            val message = intent.getStringExtra("message2").orEmpty()
+            Log.d("영역:메세지받음", message)
+            binding.getServiceMessage.text = message
+        }
+        super.onNewIntent(intent)
+    }
+
+}
+
+
+class DoitMission11Service : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            val message = intent.getStringExtra("message1").orEmpty()
+            Log.d("영역:Service", message)
+
+            Intent(this, DoitMission11::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                putExtra("message2", message)
+            }.also { intent ->
+                startActivity(intent)
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
