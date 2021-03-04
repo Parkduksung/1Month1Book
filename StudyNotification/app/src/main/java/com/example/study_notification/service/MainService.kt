76ab17c9f5728c279805a58da4355fb9a50fca65
dspark <dspark@rsupport.com>
2021-03-04 23:48:15 +0900

    [Service] 초기 세팅.

---
+++ StudyNotification/app/src/main/java/com/example/study_notification/service/MainService.kt
@@ -0,0 +1,11 @@
+package com.example.study_notification.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
