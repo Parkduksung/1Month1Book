5df7f7f6b11efba7949dfbb041893a1a98dff029
Parkduksung <duksung1234@naver.com>
2021-03-10 17:58:13 +0900

    [크롬북LifeCycle] 세팅.

---
+++ LifeCycleActivity/app/src/main/java/com/example/lifecycleactivity/ChromeBookActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lifecycleactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChromeBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chrome_book)
+
+
+
+    }
+}
