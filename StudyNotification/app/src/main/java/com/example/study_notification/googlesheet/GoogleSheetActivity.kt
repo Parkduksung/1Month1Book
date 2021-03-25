e6c19824030cd6b59aaa4eb2595ed3c82761ae47
Parkduksung <duksung1234@naver.com>
2021-03-25 14:17:02 +0900

    [googleSheet] 액티비티 생성.

---
+++ StudyNotification/app/src/main/java/com/example/study_notification/googlesheet/GoogleSheetActivity.kt
@@ -0,0 +1,13 @@
+package com.example.study_notification.googlesheet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoogleSheetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
