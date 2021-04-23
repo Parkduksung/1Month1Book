6caa59c0f4c57f172a89b7c2d19dcf1cf757e32b
Parkduksung <duksung1234@naver.com>
2021-04-23 23:54:57 +0900

    ViewModel - 기본적인 구현 및 Activity 에 생성.

--- StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceActivity.kt
+++ StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceActivity.kt
@@ -2,10 +2,21 @@ package com.example.study_notification.service
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class ServiceActivity : AppCompatActivity() {
 
+    private val serviceViewModel by lazy {
+        ViewModelProvider(this).get(ServiceViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycle.addObserver(serviceViewModel)
+    }
+
+    override fun onDestroy() {
+        lifecycle.removeObserver(serviceViewModel)
+        super.onDestroy()
     }
 }
--- StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceViewModel.kt
+++ StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.study_notification.service
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.*
 
-class ServiceViewModel : ViewModel() {
+class ServiceViewModel(private val app: Application) : AndroidViewModel(app), LifecycleObserver {
+
+    companion object {
+        private const val TAG = "ServiceViewModel"
+    }
+
+    override fun onCleared() {
+        Log.d("ServiceViewModel", "onCleared")
+        super.onCleared()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStopScreen() {
+        Log.d("ServiceViewModel", "onStopScreen")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStartScreen() {
+        Log.d("ServiceViewModel", "onStartScreen")
+    }
+
 }
