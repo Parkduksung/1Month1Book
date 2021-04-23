082e4d755da1d20e9d486a1f9849fa9e3581e644
Parkduksung <duksung1234@naver.com>
2021-04-23 23:59:43 +0900

    ViewModel - viewModel 에 MainService 등록.

--- StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceViewModel.kt
+++ StudyNotification/app/src/main/java/com/example/study_notification/service/ServiceViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.study_notification.service
 
 import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
 import android.util.Log
 import androidx.lifecycle.*
 
@@ -18,11 +23,26 @@ class ServiceViewModel(private val app: Application) : AndroidViewModel(app), Li
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStopScreen() {
         Log.d("ServiceViewModel", "onStopScreen")
+        app.unbindService(serviceConnection)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStartScreen() {
+        app.bindService(
+            Intent(app, MainService::class.java),
+            serviceConnection,
+            Context.BIND_AUTO_CREATE
+        )
         Log.d("ServiceViewModel", "onStartScreen")
     }
 
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+    }
 }
